f555388f90ff6f39f15c132d603b8731c02b47d0
treilly0607 <60261903+treilly0607@users.noreply.github.com>
2021-01-11 10:08:25 +0000

    Q1 d) Added account class and its properties and methods.

---
+++ JanExam2021/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanExam2021
+{
+    public abstract class Account
+    {
+        public string FirstName { get; set; } // first name property
+        public string Surname { get; set; } // surname property
+        public decimal Balance { get; set; } // balance property
+        public int InterestRate { get; set; } // surname property
+
+        public decimal Withdraw() // method to withdraw money
+        {
+            Balance = Balance - TransactionAmount;
+            return Balance;
+        }
+        public decimal Deposit() // method to deposit money
+        {
+            Balance = Balance + TransactionAmount;
+            return Balance;
+        }
+
+        public abstract decimal CalculateIntrest(); // absract method to calc interest
+    }
+}
